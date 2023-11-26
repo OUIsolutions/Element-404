75d21bdb0a7a6644d3a4bab7b24570a6bac25ddc
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-11-26 05:47:54 -0300

    at

--- teste.js
+++ teste.js
@@ -8,8 +8,15 @@ let e = new Element404()
 
 e.div({'test':'aa'},()=>{
      e.div(null,  "the value is" + value)
-     e.input({'onfocusout':(input)=> input_value =input.value })
-     e.text('aaaaaaaa')
+    console.log('chamou')
+
+     e.input({'focusout':(input)=> input_value  =input.value })
+     e.br()
+
+     e.text('the value of the input its'+input_value)
+     e.br()
+     e.br()
+
      e.button({'click': ()=> value-=1 },'remover')
      e.button({'click': ()=>value+=1 },'adionar')
 })
