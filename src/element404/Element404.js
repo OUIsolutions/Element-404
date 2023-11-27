6af645e86cb03f07b52576ff55f78ec4fd21402d
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-11-27 04:35:40 -0300

    att

--- src/element404/Element404.js
+++ src/element404/Element404.js
@@ -1,13 +1,15 @@
-
 /**
  * @typedef {Object} Element404
  * @property {HTMLElement} root - The root element.
  * @property {boolean} started - Indicates whether the element is started.
- * @property {function} div - The div function.
- * @property {Object} div.props - The props of the element.
- * @property {function} div.props.click - The click callback.
- * @property {function | string} div.content - The content of the element.
+
+/**
+ * @callback Element404~div
+ * @param {Object} props - The props of the element.
+ * @param {function} props.click - The click callback.
+ * @param {function | string} content - The content of the element.
  */
+
 
 /**@return {Element404} */
 function Element404(){
