30cf858f018fc30f11cf2d89db74cb68908436d3
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-11-28 16:57:38 -0300

    att

--- src/stateElements/stateInput.js
+++ src/stateElements/stateInput.js
@@ -2,20 +2,33 @@
 
 class StateInput{
     constructor(element404,name, props){
+        
         this.element404 = element404
-        this.value =this.element404.state[name]
+        this.value = null
+        let old_answer =this.element404.state[name]
+        
+        if(old_answer){
+            this.value = old_answer
+        }
+
         this.name = name
         this.props = props
     }
 
     implement(){
+
         let formated_props = {
-            value:this.value,
-            focusout: (input)=>{element404.state[this.name] = input.value}
+            focusout: (input)=>{
+                this.element404.state[this.name] = input.value
+            }
         }    
+
+        if(this.value != null){
+            formated_props[value] = this.value
+        }
         for (const key in this.props){
             formated_props[key] = this.props[key]
         }
-        this.element404.input({formated_props})
+        this.element404.input(formated_props)
     }
 }
