0898834b514c1eb027ec223e4fa631dcfb3908d7
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-12-03 06:07:37 -0300

    att

--- src/stateElements/state.js
+++ src/stateElements/state.js
@@ -6,10 +6,11 @@ class State{
         this.name = name
         this.element404 = element404
         this.value = element404.state[name]
+        
     }
     set(value){
         this.element404.state[this.name] = value
     }
-    
+
 
 }
