e7e99b45febcd585828ee18ba4aac4fd0be3909a
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-12-16 01:58:15 -0300

    at

--- Element404.js
+++ Element404.js
@@ -361,3 +361,20 @@ Element404.prototype.stateIncreaser = function(state,name,value,props,text){
     }
     this.button(formated_props,text);
 }
+Element404.prototype.stateDecreaser = function(state,name,value,props,text){
+
+    let formated_props = {
+        render_click:()=>{
+            let old_value = Number(state[name]);
+            if(!old_value){
+                old_value = 0;
+            }
+            state[name] = old_value-value;
+            this.render();
+        }
+    }
+    for(let key in props){
+        formated_props[key] = props[key];
+    }
+    this.button(formated_props,text);
+}
