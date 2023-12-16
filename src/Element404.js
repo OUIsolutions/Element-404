a7e2d02644337c50d6eaf3018e97d9fb231a7fbe
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-12-16 07:50:37 -0300

    at

--- src/Element404.js
+++ src/Element404.js
@@ -1,26 +1,42 @@
+
+ function Element404(){
+
+        /** @type {DocumentFragment} */
+        this.root;
+
+        /** @type {boolean} */
+        this.locked;
+        
+        /** @type {function} */
+        this.generator;
+
+        /** @type {HTMLElement} */  
+        this.target;
+ }
+
+
+
+
 /**
  * @param {function} generator 
  * @param {HTMLElement} target
  */
- function Element404(generator,target){
-        /** @type {DocumentFragment} */
-        this.root = document.createDocumentFragment();
-
-        /** @type {boolean} */
-        this.locked = false;
-        
-        /** @type {function} */
-        this.generator = ()=>{generator(this)}
-        /** @type {HTMLElement} */  
-        this.target = target;
- }
+Element404.prototype.rootConstructor = function(generator,target){
+     this.root = document.createDocumentFragment();
+     this.locked = false;
+     this.generator = ()=>{generator(this)}
+     this.target = target;
+}
 
  
+
 /**
  * @param {function} generator
  * @param {HTMLElement} target
  */
 function  createElement404(generator,target){
-     return new Element404(generator,target)
+     let createdd =  new Element404();
+     createdd.rootConstructor(generator,target);
+     return createdd;
 }
 
