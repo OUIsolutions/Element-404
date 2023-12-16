c9745b1b1ba8d39d473af02a4cc840c808fda7d2
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-12-16 08:24:45 -0300

    at

--- src/Element404.js
+++ src/Element404.js
@@ -1,11 +1,20 @@
 
  function Element404(){
 
-        /** @type {DocumentFragment} */
-        this.root;
+
+        /** @type {Element404} */
+        this.father = null;
+        
+        /** @type {boolean} */
+        this.child = false;
+
 
         /** @type {boolean} */
-        this.locked;
+        this.locked = false;
+
+
+        /** @type {DocumentFragment} */
+        this.root;
         
         /** @type {function} */
         this.generator;
@@ -23,12 +32,16 @@
  */
 Element404.prototype.rootConstructor = function(generator,target){
      this.root = document.createDocumentFragment();
-     this.locked = false;
      this.generator = ()=>{generator(this)}
      this.target = target;
 }
 
- 
+Element404.prototype.sub_component = function(father,root){
+     this.father = father;
+     this.root = root;
+     this.child = true;     
+} 
+
 
 /**
  * @param {function} generator
