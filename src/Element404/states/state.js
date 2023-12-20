aab703ad95abb41bea8950fd225256493662b765
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-12-20 08:50:41 -0300

    att

--- src/Element404/states/state.js
+++ src/Element404/states/state.js
@@ -1,13 +1,21 @@
 
 
+
+
 /**
- * @param {object} state
+ * @param {object} key_or_index
  * @returns {Element404}
  * */
-Element404.prototype.point_state = function(state) {
+Element404.prototype.subStateObject = function(key_or_index) {
 
+    let created =  this.stored_state[key_or_index];
+    if(!created){
+        created = {}
+        this.stored_state[key_or_index] =created
+    }
     let sub_element = new Element404();
     sub_element.sub_element(this,this.root);
-    sub_element.stored_state = state
+    sub_element.stored_state = created
     return sub_element
 }
+
