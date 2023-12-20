aecc2f6de43792791c482fde6444253a1f5f60ac
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-12-20 10:43:28 -0300

    att

--- src/Element404/states/input.js
+++ src/Element404/states/input.js
@@ -4,6 +4,7 @@
  @property {boolean=true} render_keyup
  @property {boolean=false} render_focusout
  @property {boolean=true} prevent_locker
+ @property {string  || number =null} default_value
  */
 
 
@@ -15,13 +16,14 @@
  */
 Element404.prototype.stateInput= function(name,props=null,render_props={render_keyup:true,render_focusout:false,prevent_locker:true}) {
 
-    let old_value = this.getStateValue(name);
 
     let formatted_args = new Element404Args(render_props,{});
 
     let prevent_locker =formatted_args.get('prevent_locker',true);
     let render_keyup = formatted_args.get('render_keyup',true);
     let render_focusout = formatted_args.get('render_focusout',false);
+    let default_value = formatted_args.get('default_value',"");
+    let old_value = this.getStateValue(name,default_value);
 
     let formatted_props = {
 
@@ -59,6 +61,7 @@ Element404.prototype.stateInput= function(name,props=null,render_props={render_k
     if(old_value){
         formatted_props.value = old_value
     }
+
     for(let key in props){
         formatted_props[key] = props[key];
     }
