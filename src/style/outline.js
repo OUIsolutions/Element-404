c27c3f2a48e5d80d41b71958d9138d5e52fe2d8f
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-12-26 23:33:56 -0300

    att

--- src/style/outline.js
+++ src/style/outline.js
@@ -1,8 +1,26 @@
 
+/**
+ * @typedef ComponentStyleState
+ * @property {string} state_name
+ * @property {string} css_value
+ * */
+
+/**
+ * @typedef Element404Media
+ * @property {string} media_name
+ * @property {Array<ComponentStyleState>} states
+ *
+ * */
 class  Element404Outline{
-   
+    /**@type {Array<Element404Media>}*/
     medias = [];
 
+
+    /**
+    * @param {string || undefined} media_name
+     * @param {string || undefined} state_name
+    *@return {ComponentStyleState}
+    * */
     get_element(media_name,state_name){
         let media_element = undefined;
         let current_state = undefined;
@@ -15,20 +33,62 @@ class  Element404Outline{
         }
 
         if(!media_element){
-            media_element = {media_name:media_name,component_states:[]};
+            media_element = {media_name:media_name,states:[]};
             this.medias.unshift(media_element);
         }
 
-        for(let state of media_element.component_states){
+        for(let state of media_element.states){
             if(state.state_name === state_name){
                 current_state = state;
             }
         }
         if(!current_state){
-            media_element.component_states.unshift({state_name:state_name,css_value:""})
+            current_state = {state_name:state_name,css_value:""};
+            media_element.states.unshift(current_state)
+
         }
         return current_state;
     }
 
-    
+    /**
+     * @param {string || undefined} media_name
+     * @param {string || undefined} state_name
+     * @param {string} value
+     * */
+    set_text(media_name,state_name,value){
+        let media = this.get_element(media_name,state_name);
+        media.css_value+= value
+
+    }
+    /**
+     * @param {number} identifier
+     * @return {string}
+     * */
+    render(identifier){
+        let final_text = ''
+        for(let media of this.medias){
+
+            let media_name = media.media_name;
+            if(media_name){
+                    final_text+=`${media_name} {`
+            }
+
+            for(let state of media.states){
+                final_text+=`[Element404Identifier="${identifier}"] `
+                let state_name = state.state_name;
+                if(state_name){
+                    state_name = state_name.replace(":","");
+                    final_text+=`:${state_name}`
+                }
+                final_text+=`{${state.css_value}}`
+            }
+
+
+            if(media_name){
+                final_text+="}"
+
+            }
+        }
+    }
+
 }
